602e2d8ac1a399e5aeed351d6c6cef95ef7be22a
richod1 <frimpsup@gmail.com>
2022-10-18 06:32:14 -0700

    ADD:Detail.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import ImgSlider from './ImgSlider'
 import Viewers from './Viewers'
+import Movies from './Movies'
 
 
 function Home() {
@@ -9,6 +10,7 @@ function Home() {
     <Container>
       <ImgSlider/>
       <Viewers/>
+      <Movies/>
      
     </Container>
   )
